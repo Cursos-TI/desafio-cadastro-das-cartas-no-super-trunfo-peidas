6e6f20abc1aa98701316ca01724253543a1147d6
cacastro <caiqueca213@gmail.com>
2025-08-12 20:45:03 +0000

    adicionei as variaveis da segunda carta juntamente com parte do cadastro da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 
 int main (){
-    int população, pontosT;
-    float area, pib;
-    char estado;
-    char codigo[3];
-    char nome[50];
+    int população1, pontosT1;
+    float area1, pib1;
+    char estado1;
+    char codigo1[3];
+    char nome1[50];
+
+    int população2, pontosT2;
+    float area2, pib2;
+    char estado2;
+    char codigo2[3];
+    char nome2[50];
+
+    //anotação para facilitar na hora de codar: 
+%d para int, %f para float, %c para char e %s para char[]
+
+    printf("-- Cadastro da primeira carta --");
+
+    printf("Estado: \n");
+    scanf(" %c", &estado1);
+
+    printf("Código da carta: \n");
+    scanf("%s", codigo1);
+
+    printf("Nome da cidade: \n");
+    scanf("%s", &nome1);
+
+    printf("Populaçao da cidade: \n");
+    scanf("%d", população1)
 
     return 0;
 
