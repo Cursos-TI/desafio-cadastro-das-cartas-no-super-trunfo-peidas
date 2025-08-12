8a5f07f393aa48ce4916a7a0a430d700aa6790c6
cacastro <caiqueca213@gmail.com>
2025-08-12 20:22:17 +0000

    adicionei os codigos de informação para a primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    
+int main (){
+    int população, pontosT;
+    float area, pib;
+    char estado;
+    char codigo[3];
+    char nome[50];
+
     return 0;
+
 }
