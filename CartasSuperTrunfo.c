278027fde42f8962fdc7db4250332c0188aa8b3e
cacastro <caiqueca213@gmail.com>
2025-08-14 21:26:56 +0000

    finalizei os codigos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main (){
     int população1, pontosT1;
     float area1, pib1;
     char estado1, codigo1[4], nome1[50];
-
+    //coloquei tudo junto pra ser mais facil de ler
     int população2, pontosT2;
     float area2, pib2;  
     char estado2, codigo2[4], nome2[50];
@@ -17,10 +17,10 @@ int main (){
     scanf(" %c", &estado1);
 
     printf("Código da carta: \n");
-    scanf("%s", &codigo1);
+    scanf("%s", codigo1);
 
     printf("Nome da cidade: \n");
-    scanf("%s", &nome1);
+    scanf("%s", nome1);
 
     printf("Populaçao da cidade: \n");
     scanf("%d", &população1);
@@ -34,13 +34,15 @@ int main (){
     printf("Número de pontos turísticos: \n");
     scanf("%d", &pontosT1);
 
+    //fim do cadastro da primeira carta
+
     printf("-- Cadastro da segunda carta --\n");
 
     printf("Estado: \n");
     scanf(" %c", &estado2);
 
     printf("Código da carta: \n");
-    scanf("%s", &codigo2);
+    scanf("%s", codigo2);
 
     printf("Nome da cidade: \n");
     scanf("%s", nome2);
@@ -56,6 +58,18 @@ int main (){
 
     printf("Número de pontos turísticos: \n");
     scanf("%d", &pontosT2);
+
+    //fim do cadastro da segunda carta
+
+    printf(" -- Dados da primeira carta: -- \n"); 
+    printf("Estado:  %c \nCódigo da carta: %s \nNome da Cidade: %s \n", estado1, codigo1, nome1);
+    printf("População da Cidade: %d \nÁrea da Cidade em km²: %f \nPIB da Cidade: %f \n", população1, area1, pib1);
+    printf("Número de Pontos Turísticos da cidade %d \n", pontosT1);
+
+    printf(" -- Dados da segunda carta: -- \n"); 
+    printf("Estado:  %c \nCódigo da carta: %s \nNome da Cidade: %s \n", estado2, codigo2, nome2);
+    printf("População da Cidade: %d \nÁrea da Cidade em km²: %f \nPIB da Cidade: %f \n", população2, area2, pib2);
+    printf("Número de Pontos Turísticos da cidade %d \n", pontosT2);
 
     return 0;
 
